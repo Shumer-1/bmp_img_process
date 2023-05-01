391e184d2bb5dfe669e880cb252bedfcca0036c2
Shumer-1 <borisov1908vlad@mail.ru>
2023-05-01 20:53:58 +0300

    plus reflection, but may be false

--- My_things/copy_reflection.c
+++ My_things/copy_reflection.c
@@ -27,5 +27,29 @@ void copy(BMP* img, Point p1, Point p2, Point p_){
 
 void reflection(BMP* img, Line line, Point p1, Point p2){
     // запишем уравнение прямой
-    
+    Point new_p1; // нижняя левая точка (принцип зеркала)
+    Point new_p2; // правая верхняя
+    if (line.p1.x1 == line.p2.x1){
+        new_p1.y1 = (line.p1.y1 - p1.y1) + line.p1.y1; // новая y это разность и еще точка отражения
+        new_p1.x1 = p1.x1;
+        new_p2.y1 = (line.p2.y1 - p2.y1) + line.p2.y1;
+        new_p2.x1 = p2.x1;
+        Point usefull;
+        usefull.x1 = new_p1.x1;
+        usefull.y1 = new_p2.y1;
+        copy(img, p1, p2, usefull);
+    }
+    else if (line.p1.y1 == line.p2.y1){
+        new_p1.x1 = (line.p1.x1 - p1.x1) + line.p1.x1;
+        new_p1.y1 = p1.y1;
+        new_p2.x1 = (line.p2.x1 - p2.x1) + line.p2.x1;
+        new_p2.y1 = p2.y1;
+        Point usefull;
+        usefull.x1 = new_p2.x1;
+        usefull.y1 = new_p1.y1;
+        copy(img, p1, p2, usefull);
+    }
+    else{
+        puts("Прямая не горизонтальная и не вертикальная, такую приколдесу я еще не сделал");
+    }
 }
